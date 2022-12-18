08e1e9933454716f25d2b7d073a0cc5e44a28439
hnquy156 <quyhn@mioapp.vn>
2022-12-18 20:20:11 +0700

    init graph server

---
+++ app.js
@@ -0,0 +1,27 @@
+const { ApolloServer, gql } = require('apollo-server-express');
+const express = require('express');
+const resolvers = require('./resolver/resolver');
+const typeDefs = require('./schema/schema');
+
+startApolloServer();
+
+async function startApolloServer() {
+  const app = express();
+  const server = new ApolloServer({
+    typeDefs,
+    resolvers,
+  });
+  await server.start();
+
+  server.applyMiddleware({ app });
+
+  app.use((req, res) => {
+    res.status(200);
+    res.send('Hello!');
+    res.end();
+  });
+
+  await new Promise((resolve) => app.listen({ port: 4000 }, resolve));
+  console.log(`🚀 Server ready at http://localhost:4000${server.graphqlPath}`);
+  return { server, app };
+}
